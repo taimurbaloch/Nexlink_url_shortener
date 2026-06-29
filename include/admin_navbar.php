6308f41d86f10589a5e3dae010fbbbf9c7d9f782
taimurbaloch <taimurbaloch583@gmail.com>
2026-06-29 13:39:09 +0500

    Add admin navigation bar to admin_navbar.php

---
+++ include/admin_navbar.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-lg bg-white sticky-top shadow-sm">
+    <div class="container">
+
+        <a class="navbar-brand fw-bold" href="../index.php">
+            <i class="bi bi-link-45deg text-primary"></i>
+            NexLink
+        </a>
+
+        <div>
+            <a href="../index.php" class="btn btn-outline-primary me-2">
+                Home
+            </a>
+
+            <a href="../about.php" class="btn btn-outline-secondary me-2">
+                About
+            </a>
+
+            <a href="dashboard.php" class="btn btn-primary">
+                Dashboard
+            </a>
+        </div>
+
+    </div>
+</nav>
