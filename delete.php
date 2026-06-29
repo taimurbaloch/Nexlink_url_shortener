f9cb215dff4a0208fab445c57df15cae2420679e
taimurbaloch <taimurbaloch583@gmail.com>
2026-06-29 13:20:27 +0500

    Delete delete.php

--- delete.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$url_id= $_GET['id'];
-
-$conn=new mysqli('localhost','root','','url_shortener');
-$q='delete from url where id='.$url_id;
-$conn->query($q);
-header('location:../admin1/urls.php');
-?>
